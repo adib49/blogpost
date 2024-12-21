c9cd1a3122abfdc36d72488b5b72b218f601de98
Adib M <adibw449@gmail.com>
2024-12-21 20:32:28 +0530

    fix gitignore and remove tracked files

--- BlogPost/urls.py
+++ BlogPost/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path
 from blog.views import *
 from django.conf import settings
-from django.conf.urls import static
+from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
